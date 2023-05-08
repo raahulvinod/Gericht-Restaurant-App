592f3993ce8e5870df99fc9d1ac5876eefc9ac62
Rahul vinod <120365941+raahulvinod@users.noreply.github.com>
2023-05-08 19:22:06 +0530

    award card content

--- src/container/Laurels/Laurels.jsx
+++ src/container/Laurels/Laurels.jsx
@@ -1,10 +1,23 @@
 import React from 'react';
 
+import { SubHeading } from '../../components';
+import { images, data } from '../../constants';
+
 import './Laurels.css';
+const AwardCard = () => {};
 
 const Laurels = () => (
-  <div>
-    Laurels
+  <div className="app__bg app__wrapper section__padding" id="awards">
+    <div className="app__wrapper_info">
+      <SubHeading title="Awards & recognition" />
+      <h1 className="headtext__cormorant">Our Laurels</h1>
+      <div className="app__laurals_awards">
+        {data.awards.map((award) => (
+          <AwardCard award={award} key={award.title} />
+        ))}
+      </div>
+    </div>
+    <div className="app__wrapper_img"></div>
   </div>
 );
 
